df39a9ff6b1d242e32818b400a04a2a0d9030e1a
Datom-code <enochgbadebo007@gmail.com>
2026-07-30 09:36:18 -0400

    Add user authentication endpoints for registration and login

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterUserView
 
 urlpatterns = [
     # endpoint for obtaining JWT tokens
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),      
-     
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # endpoint for obtaining JWT tokens
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # endpoint for refreshing JWT tokens
+
+    path('auth/register/', RegisterUserView.as_view(), name='register_user'),  # endpoint for user registration
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # endpoint for user login
+
+
 ]
