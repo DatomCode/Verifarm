0c7a051fef6593d1306767384245cf166a8c60d7
Datom-code <enochgbadebo007@gmail.com>
2026-07-31 10:13:26 -0400

    Fix import path for settings in models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-from backend.verifarm import settings
+from verifarm import settings
 
 # Create your models here.
 
