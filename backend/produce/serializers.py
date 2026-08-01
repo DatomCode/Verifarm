f64acd5d2a2c80e154bf83e378c3eb9478af16ec
Datom-code <enochgbadebo007@gmail.com>
2026-08-01 17:56:47 -0400

    Update ProduceBatchSerializer to include location field and restore create method

--- backend/produce/serializers.py
+++ backend/produce/serializers.py
@@ -4,11 +4,14 @@ from .models import ProduceBatch
 class ProduceBatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProduceBatch
-        fields = ["commodity_type", "quantity", "status", "created_at", "updated_at"]
+        fields = ["commodity_type", "quantity", "location", "status", "created_at", "updated_at"]
 
-    #  def create(self, validate):
-    #     produce_batch = ProduceBatch.objects.create(
-            
-    #     )
-
-
+    def create(self, validated_data):
+        produce_batch = ProduceBatch.objects.create(
+            farmer_profile=self.context["request"].user.farmer_profile,
+            commodity_type=validated_data["commodity_type"],
+            quantity=validated_data["quantity"],
+            location=validated_data["location"],
+            status=validated_data["status"]
+        )
+        return produce_batch
